7b3c61d29663058ecfe55179bfb18376c7dd76bb
Kenny Johnston <kjohnston.ca@gmail.com>
2011-02-28 20:44:50 -0800

    Set from address on tracer bullet message to sender set in ActionMailer config.

--- lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
+++ lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
@@ -3,7 +3,7 @@ class DelayedJobTracerMailer < ActionMailer::Base
   def delayed_job_tracer_test_message
     config  = YAML.load_file(Rails.root.join('config', 'delayed_job_tracer_config.yml'))
     to      = config['monitor']['username']
-    from    = config['alert']['username']
+    from    = ActionMailer::Base.smtp_settings[:user_name]
     subject = "[DelayedJobTracer] Test message sent at: #{Time.zone.now}"
     message = 'This is a test message to ensure messages are being delivered successfully via delayed_job.'
     mail(:to => to, :from => from, :subject => subject) do |format|

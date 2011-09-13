7bd23514d94b3df06056969d40f733f8a184ef6c
Kenny Johnston <kjohnston.ca@gmail.com>
2011-09-12 22:09:27 -0700

    Configure DelayedJobTracerMailer to use ActionMailer 2.3 style syntax.

--- lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
+++ lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
@@ -1,18 +1,16 @@
 class DelayedJobTracerMailer < ActionMailer::Base
 
   def delayed_job_tracer_test_message
-    config  = YAML.load_file(Rails.root.join('config', 'delayed_job_tracer_config.yml'))
-    to      = config['monitor']['username']
-    from    = ActionMailer::Base.smtp_settings[:user_name]
-    subject = "[DelayedJobTracer] Test message sent at: #{Time.zone.now}"
-    message = 'This is a test message to ensure messages are being delivered successfully via delayed_job.'
-    mail(:to => to, :from => from, :subject => subject) do |format|
-      format.text { render :text => message }
-    end
+    config      = YAML.load_file(Rails.root.join('config', 'delayed_job_tracer_config.yml'))
+    @from       = ActionMailer::Base.smtp_settings[:user_name]
+    @recipients = config["monitor"]["username"]
+    @subject    = "[DelayedJobTracer] #{Time.zone.now}"
+    @sent_on    = Time.zone.now
+    @body       = "This is a test message to ensure messages are being delivered successfully via delayed_job."
   end
-  
+
   def self.enqueue_delayed_job_tracer_test_message
-    delay.delayed_job_tracer_test_message
+    delay.deliver_delayed_job_tracer_test_message
   end
-  
+
 end
